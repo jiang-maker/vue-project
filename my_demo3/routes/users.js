5c25df05fa4dbbba1131adf7cdf9436c9ab31ef3
rambo <137764155@qq.com>
2020-04-01 20:35:46 +0800

    mongo

--- my_demo3/routes/users.js
+++ my_demo3/routes/users.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'root',
-  database:'server'
-});
-connection.connect();
 
-/* Post method for users listening. */
-router.post('/userinfo', function (req, res, next) {
-  console.log('请求信息为：');
-  console.log(req.body);
-  res.json({
-    meta:{
-      code: '200'
-    },
-    data: {
-      message:'dfasdfsfs'
-    }
-  })
-})
-/* GET users listening. address:localhost:3000/users/test */
-router.get('/test', function (req, res, next) {
-  res.json({
-    meta:{
-      code: '200'
-    },
-    data: {
-      message:'dfasdfsfs'
-    }
-  })
-})
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
